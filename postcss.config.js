9626158c12d04d059f76c1ef25a277e73abac58b
minnn311 <m1210891@0365.fcu.edu.tw>
2025-08-12 13:53:36 +0800

    新增demo yt link

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-  ],
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
 
